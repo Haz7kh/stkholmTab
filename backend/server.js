f39ca64c1edf7a6416cd5dcb10f818edcd0fe68a
Khaled <kh.almelhem@gmail.com>
2024-06-07 12:39:37 +0200

    newvadd

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 5001;
 
 // Middleware
-app.use(cors());
+app.use(cors({}));
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
